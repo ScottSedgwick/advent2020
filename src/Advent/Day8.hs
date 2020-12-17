20a0dd4fd88a38751757155ba2bf3e870834f12b
Scott Sedgwick <ssedgwick@cochlear.com>
2020-12-17 21:55:56 +1100

    Improved Day 8

--- src/Advent/Day8.hs
+++ src/Advent/Day8.hs
@@ -4,7 +4,7 @@ module Advent.Day8
   ( day8pt1,
     day8pt2,
     day8parser,
-    Term (..),
+    Termination (..),
   )
 where
 
@@ -20,7 +20,9 @@ data Action
   | Nop
   deriving stock (Show)
 
-day8parser :: Parser [Action]
+type Program = [Action]
+
+day8parser :: Parser Program
 day8parser = many p
   where
     p = do
@@ -35,7 +37,7 @@ day8parser = many p
         "jmp" -> Jmp value'
         _ -> Nop
 
-data Term
+data Termination
   = NoInstructions
   | IndexTooLow
   | IndexTooHigh
@@ -44,33 +46,34 @@ data Term
   | NoValidProgram
   deriving stock (Show, Eq)
 
-day8pt1 :: [Action] -> Term
-day8pt1 = runProgram [] 0 0
+day8pt1 :: Program -> Termination
+day8pt1 = runProgram
 
-runProgram :: [Int] -> Int -> Int -> [Action] -> Term
-runProgram _ _ _ [] = NoInstructions
-runProgram ds index acc xs
-  | index < 0 = IndexTooLow
-  | index > length xs = IndexTooHigh
-  | index == length xs = Normal acc
-  | index `elem` ds = InfiniteLoop acc
-  | otherwise = runProgram (index : ds) index' acc' xs
+runProgram :: Program -> Termination
+runProgram [] = NoInstructions
+runProgram xs = rp [] 0 0 xs
   where
-    (index', acc') = exec index acc xs
-    exec i a ys =
-      case ys !! i of
-        Acc x -> (i + 1, a + x)
-        Jmp x -> (i + x, a)
-        Nop -> (i + 1, a)
+    rp ds index acc ys
+      | index < 0 = IndexTooLow
+      | index > length ys = IndexTooHigh
+      | index == length ys = Normal acc
+      | index `elem` ds = InfiniteLoop acc
+      | otherwise = rp (index : ds) index' acc' ys
+      where
+        (index', acc') =
+          case ys !! index of
+            Acc x -> (index + 1, acc + x)
+            Jmp x -> (index + x, acc)
+            Nop   -> (index + 1, acc)
 
-day8pt2 :: [Action] -> Term
-day8pt2 = fromMaybe NoValidProgram . find isNormal . map (runProgram [] 0 0) . programPermutations 0
+day8pt2 :: Program -> Termination
+day8pt2 = fromMaybe NoValidProgram . find isNormal . map runProgram . programPermutations 0
 
-isNormal :: Term -> Bool
+isNormal :: Termination -> Bool
 isNormal (Normal _) = True
 isNormal _ = False
 
-programPermutations :: Int -> [Action] -> [[Action]]
+programPermutations :: Int -> Program -> [Program]
 programPermutations i xs
   | i >= length xs = []
   | otherwise = f : programPermutations (i + 1) xs
