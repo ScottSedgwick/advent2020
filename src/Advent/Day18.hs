bddffc152aad4a9401f428c3ec435df44615b097
Scott Sedgwick <ssedgwick@cochlear.com>
2020-12-18 21:57:11 +1100

    Improved Day 18

--- src/Advent/Day18.hs
+++ src/Advent/Day18.hs
@@ -1,25 +1,27 @@
 module Advent.Day18
-  ( day18pt1
-  , day18pt2
-  ) where
-  
-import Control.Monad.Combinators.Expr ( Operator(..), makeExprParser )
-import Data.Default                   ( Default(..) )
-import Text.Megaparsec                ( (<|>) )
-import Advent.ParseUtils              ( Parser, integer, parens, parseString, symbol )
+  ( day18pt1,
+    day18pt2,
+  )
+where
+
+import Advent.ParseUtils (Parser, integer, parens, parseString, symbol)
+import Control.Monad.Combinators.Expr (Operator (..), makeExprParser)
+import Data.Default (Default (..))
+import Text.Megaparsec ((<|>))
 
 data Node = Val Integer | Sum Node Node | Pro Node Node deriving stock (Eq, Show)
+
 instance Default Node where
   def = Val 0
 
 expr1 :: Parser Node
-expr1 = makeExprParser (parens expr1 <|> (Val <$> integer)) [ [ InfixL (Sum <$ symbol "+"), InfixL (Pro <$ symbol "*") ] ]
+expr1 = makeExprParser (parens expr1 <|> (Val <$> integer)) [[InfixL (Sum <$ symbol "+"), InfixL (Pro <$ symbol "*")]]
 
 expr2 :: Parser Node
-expr2 = makeExprParser (parens expr2 <|> (Val <$> integer)) [ [ InfixL (Sum <$ symbol "+") ], [ InfixL (Pro <$ symbol "*") ] ]
+expr2 = makeExprParser (parens expr2 <|> (Val <$> integer)) [[InfixL (Sum <$ symbol "+")], [InfixL (Pro <$ symbol "*")]]
 
 eval :: Node -> Integer
-eval (Val x)   = x
+eval (Val x) = x
 eval (Sum a b) = eval a + eval b
 eval (Pro a b) = eval a * eval b
 
