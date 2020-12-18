6de69f46b8ed387b4a68e9a6a4bd1952f33789cf
Scott Sedgwick <ssedgwick@cochlear.com>
2020-12-18 21:48:34 +1100

    Improved Day 18

--- src/Advent/Day18.hs
+++ src/Advent/Day18.hs
@@ -3,44 +3,31 @@ module Advent.Day18
   , day18pt2
   ) where
   
-import Control.Monad.Combinators.Expr ( makeExprParser, Operator(..) )
-import Data.Default ( Default(..) )
-import Text.Megaparsec ( (<|>), (<?>) )
-import Advent.ParseUtils ( integer, Parser, symbol, parens, parseString )
+import Control.Monad.Combinators.Expr ( Operator(..), makeExprParser )
+import Data.Default                   ( Default(..) )
+import Text.Megaparsec                ( (<|>), (<?>) )
+import Advent.ParseUtils              ( Parser, integer, parens, parseString, symbol )
 
-data Node
-  = Val Integer
-  | Sum Node Node
-  | Pro Node Node
-  deriving stock (Eq, Show)
-
+data Node = Val Integer | Sum Node Node | Pro Node Node deriving stock (Eq, Show)
 instance Default Node where
   def = Val 0
 
 expr1 :: Parser Node
-expr1 = makeExprParser term1 table1
-  where
-    table1 = [ [ InfixL (Pro <$ symbol "*")
-               , InfixL (Sum <$ symbol "+") 
-               ] 
-             ]
-    term1  = parens expr1 <|> (Val <$> integer) <?> "term"
+expr1 = makeExprParser (parens expr1 <|> (Val <$> integer) <?> "term") [ [ InfixL (Pro <$ symbol "*"), InfixL (Sum <$ symbol "+") ] ]
 
 expr2 :: Parser Node
-expr2 = makeExprParser term2 table2
-  where
-    table2 = [ [ InfixL (Sum <$ symbol "+") ]
-             , [ InfixL (Pro <$ symbol "*") ]
-             ]
-    term2  = parens expr2 <|> (Val <$> integer) <?> "term"
+expr2 = makeExprParser (parens expr2 <|> (Val <$> integer) <?> "term") [ [ InfixL (Sum <$ symbol "+") ], [ InfixL (Pro <$ symbol "*") ] ]
 
 eval :: Node -> Integer
 eval (Val x)   = x
 eval (Sum a b) = eval a + eval b
 eval (Pro a b) = eval a * eval b
 
+execute :: Parser Node -> [String] -> Integer
+execute f = sum . map (eval . parseString f)
+
 day18pt1 :: [String] -> Integer
-day18pt1 = sum . map (eval . parseString expr1)
+day18pt1 = execute expr1
 
 day18pt2 :: [String] -> Integer
-day18pt2 = sum . map (eval . parseString expr2)
+day18pt2 = execute expr2
